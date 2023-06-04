c9aad74627d80851dd16661e0bcb27f6ce1f5753
Zuriel Zavala <zzavala2@hotmail.com>
2023-06-04 00:22:47 -0500

    6/4/23
    worked on score script

--- GAM392/Assets/Scripts/UI/UI_Score.cs
+++ GAM392/Assets/Scripts/UI/UI_Score.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI_Score : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Update score by adding increment method in Enemy class to 
+    private int score; //score value reference
+
+    private SpriteRenderer scoreSprite; //score sprite reference 
     void Start()
     {
-        
+        score = 0; //initalize score to 1
     }
 
     // Update is called once per frame
